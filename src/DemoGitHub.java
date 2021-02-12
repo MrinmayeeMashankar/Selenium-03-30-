e8b3af4fb428ec660a2f79828c037d2516dd8324
Hp <Hp@Hp-PC>
2021-02-12 14:47:54 +0530

    2nd Commit

---
+++ src/DemoGitHub.java
@@ -0,0 +1,21 @@
+
+public class DemoGitHub 
+{
+	int a=10;
+	int b=20;
+	int c=0;
+	
+	void addition()
+	{
+		System.out.println("a value is "+a);
+		System.out.println("b value is "+b);
+		c=a+b;
+		System.out.println("Addition result is "+c);
+	}
+	
+public static void main(String[] args)
+{
+	DemoGitHub d1=new DemoGitHub();
+	d1.addition();
+}
+}
